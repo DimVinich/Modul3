b49fe69e9719b7c5c2dce85b8568a609a2a639d8
DimVinich <DmitrijVS@gmail.com>
2022-05-11 22:38:25 +0300

    Задание 3.3.2

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -6,15 +6,19 @@ namespace Modul3
     {
         static void Main(string[] args)
         {
-			const string MyName = "Jane";
+			string MyName = "Dmitrij";
+			byte MyAge = 50;
+			bool HavePet = false;
+			double MyShoeSize = 42;
 
-			Console.WriteLine(MyName);
+			Console.WriteLine("My name is {0}", MyName);
+			Console.WriteLine("My Age is {0}", MyAge);
+			Console.WriteLine("Do I have a pate? "+HavePet);
+			Console.WriteLine("My shoe size is " + MyShoeSize);
 
-			Console.WriteLine("\t Привет Мир");
-			Console.WriteLine("\t Мне 27 лет");
-			Console.WriteLine("\t My name is \n {0}", MyName);
-			Console.WriteLine("\u0040");
-			Console.WriteLine("\u0023");
+
+			//Console.WriteLine("\u0040");
+			//Console.WriteLine("\u0023");
 			//			Console.ReadKey();
 		}
 	}
