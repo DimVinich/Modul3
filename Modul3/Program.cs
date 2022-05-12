482353928ae0f792913d63eea88974ce90f3defc
DimVinich <DmitrijVS@gmail.com>
2022-05-12 19:42:29 +0300

    Весь раздел 3.5

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -7,6 +7,33 @@ namespace Modul3
         static void Main(string[] args)
         {
 			string MyName = "Dmitrij";
+			string name = "Dmitrij";
+			byte age = 50;
+			string favcolor = "Зелёный";
+			byte weight = 80;
+
+			Console.WriteLine("\t Привет,\n мир!");
+			Console.WriteLine();
+			Console.WriteLine($"Меня зовут {MyName}");
+			Console.WriteLine($"Мой возраст {age}");
+
+			Console.WriteLine("{0} \n\t {1} \n {2}", name, age,  favcolor);
+
+			string str =Console.ReadLine();
+		}
+
+
+		enum Semaphore
+		{
+			Red = 100,
+			Yellow = 200,
+			Green = 300
+		}
+
+	}
+}
+
+/*
 			byte MyAge = 50;
 			bool HavePet = false;
 			double MyShoeSize = 42;
@@ -16,10 +43,8 @@ namespace Modul3
 			Console.WriteLine("Do I have a pate? "+HavePet);
 			Console.WriteLine("My shoe size is " + MyShoeSize);
 
+*/
+//Console.WriteLine("\u0040");
+//Console.WriteLine("\u0023");
+//			Console.ReadKey();
 
-			//Console.WriteLine("\u0040");
-			//Console.WriteLine("\u0023");
-			//			Console.ReadKey();
-		}
-	}
-}
